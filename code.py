27bd0ae91943fc771a7bc8a7a91febdabf306e2d
Batul765 <151876174+Batul765@users.noreply.github.com>
2023-11-23 23:16:43 +0300

    Update code.py

--- code.py
+++ code.py
@@ -12,8 +12,8 @@ y = data["churned"]
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# Train the logistic regression model
-model = LogisticRegression()
+# Train the Random Forest model
+model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
 # Make predictions on the test set
